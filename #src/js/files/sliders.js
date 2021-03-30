4e457e2638970315f3ab53abb13d805389dfe867
«Alexandr <alkurto792@gmail.com>
2021-03-30 17:16:50 +0300

    7. Slider Lots

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -119,3 +119,64 @@ let main_slider = new Swiper('.main-slider__body', {
 	//	el: '.swiper-scrollbar',
 	//},
 });
+
+let slider_lots = new Swiper('.slider-lots__body', {
+	/*
+	effect: 'fade',
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	*/
+	observer: true,
+	observeParents: true,
+	slidesPerView: 3,
+	spaceBetween: 0,
+	// autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	loop: true,
+	//preloadImages: false,
+	lazy: true,
+	// Dotts
+	//pagination: {
+	//	el: '.slider-quality__pagging',
+	//	clickable: true,
+	//},
+	// Arrows
+	navigation: {
+		nextEl: '.control-slider-lots__arrow_next',
+		prevEl: '.control-slider-lots__arrow_prev',
+	},
+	/*
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1268: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
+	*/
+	on: {
+		lazyImageReady: function () {
+			ibg();
+		},
+	}
+	// And if we need scrollbar
+	//scrollbar: {
+	//	el: '.swiper-scrollbar',
+	//},
+});
